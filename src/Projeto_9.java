266383f129579c71fdf0a42c9685e572f1a9b821
unknown <dpaiva266@hotmail.com>
2024-02-20 15:54:20 -0300

    *Tarefa modulo 9

---
+++ src/Projeto_9.java
@@ -0,0 +1,23 @@
+
+
+public class Projeto_9 {
+
+    public static void main(String args[]){
+
+    int nun1 = 10;
+    int nun2 = nun1;
+
+
+    long nun3 = 1037458795l;
+    int nun4 = (int) nun3;
+
+
+        System.out.println(nun2);
+        System.out.println(nun4);
+}
+
+
+
+
+}
+
